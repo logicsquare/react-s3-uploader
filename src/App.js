1b686482ae94a49286db4b1f3a821f2ab68aa5d5
debojyoti2 <debojyoti@logic-square.com>
2020-01-29 18:13:59 +0530

    Initial upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as AWS from "aws-sdk";
+import S3BucketUploader from "./s3-bucket-uploader";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    file: "",
+    isUploading: false,
+    uploadPercentage: 0
+  };
+
+  s3Uploader = null;
+
+  async componentDidMount() {
+    const config = await S3BucketUploader.getCreds(
+      "https://48f73f27.ngrok.io/awstempcreds"
+    );
+    //  Initialize S3 Uploader
+    this.s3Uploader = new S3BucketUploader(config);
+  }
+
+  _updateFile = e => {
+    let { file } = this.state;
+    if (e && e.target && e.target.files && e.target.files.length) {
+      file = e.target.files[0];
+      this.setState({ file });
+    }
+  };
+
+  _uploadFile = () => {
+    let { file } = this.state;
+    this.s3Uploader.uploadFile(file, this._onComplete, this._onUploadProgress);
+  };
+
+  _onUploadProgress = evt => {
+    let {uploadPercentage, isUploading} = this.state;
+    isUploading = true;
+    uploadPercentage = parseInt((evt.loaded * 100) / evt.total) + '%';
+    this.setState({ uploadPercentage, isUploading });
+  }
+
+  _onComplete = (error, success) => {
+    console.log('error, success :', error, success);
+  }
+
+  render() {
+    const { uploadPercentage, isUploading } = this.state;
+    return (
+      <>
+        <input
+          type="file"
+          name="file"
+          onChange={this._updateFile}
+        />
+        <button type="button" onClick={this._uploadFile}>
+          Upload
+        </button>
+        {
+          isUploading? (
+          <p>Upload Progress: {uploadPercentage}</p>
+          ): null
+        }
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/s3-bucket-uploader/index.js
@@ -0,0 +1,64 @@
+import { config as AWSConfig, S3 } from "aws-sdk";
+
+class S3BucketUploader {
+  /**
+   * Static properties
+   */
+  static config = null;
+
+  static setConfigs(config) {
+    S3BucketUploader.config = config;
+  }
+
+  static getCreds = url => {
+    return new Promise(async (resolve, reject) => {
+      fetch(url, {
+        method: "GET"
+      })
+        .then(res => {
+          return res.json();
+        })
+        .then(res => {
+          resolve(S3BucketUploader.prepareAWSConfigs(res));
+        });
+    });
+  };
+
+  static prepareAWSConfigs = config => {
+    return {
+      accessKeyId: config.AccessKeyId,
+      secretAccessKey: config.SecretAccessKey,
+      sessionToken: config.SessionToken,
+      S3BucketName: config.S3BucketName,
+      S3Region: config.S3Region
+    };
+  };
+
+  /**
+   * Non static properties
+   */
+
+  constructor(config) {
+    S3BucketUploader.setConfigs(config);
+  }
+
+  uploadFile = (file, onComplete, onProgressCallBack = () => {}) => {
+    const { config } = S3BucketUploader;
+    AWSConfig.update(config);
+    // Configure your region
+    AWSConfig.region = config.S3Region;
+    var bucket = new S3({
+      params: {
+        Bucket: config.S3BucketName
+      }
+    });
+    const Key = String(+new Date());
+    var params = { Key, ContentType: file.type, Body: file };
+    bucket
+      .upload(params)
+      .on("httpUploadProgress", onProgressCallBack)
+      .send(onComplete);
+  };
+}
+
+export default S3BucketUploader;
